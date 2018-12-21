332ec63de4c798536f84c4568984b76c5d83e0ad
Madserker <madserker@gmail.com>
2018-12-21 09:55:11 +0100

    stable version

--- redux-cra/src/js/actions/getActions.js
+++ redux-cra/src/js/actions/getActions.js
@@ -1,6 +1,7 @@
 import{ FETCH_GNOMES } from './types';
 
-export const fetchGnomes = () => dispatch => {
+export function fetchGnomes(){
+    return function(dispatch){
         fetch('https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json')
         .then(res => res.json())
         .then(gnomes =>
@@ -8,17 +9,38 @@ export const fetchGnomes = () => dispatch => {
                 type:FETCH_GNOMES,
                 payload: gnomes.Brastlewark
             }));
+    }
 }
 
-//SEARCH FILTERS=================================================
-export const fetchGnomesFilters = () => dispatch => {
-    fetch('https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json')
-    .then(res => res.json())
-    .then(gnomes =>
-        dispatch({
-            type:FETCH_GNOMES,
-            payload: gnomes.Brastlewark
-        }));
+
+//SEARCH FILTERS ACTIONS=================================================
+//Se puede aplicar mas de un filtro a la vez,
+//Por eso necesitamos aplicar estos filtros a la lista que tenemos en state de store
+//tambien ordenamos la lista que retornamos de los filtros, y no la lista original
+export function fetchGnomesFilters(){
+    return function(dispatch){
+        fetch('https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json')
+        .then(res => res.json())
+        .then(gnomes => filterByHairColor("Pink",gnomes.Brastlewark))
+        .then(gnomes =>
+            dispatch({
+                type:FETCH_GNOMES,
+                payload: gnomes
+            }));
+}
+}
+
+//SEARCH FILTERS FUNCTIONS=================================================
+function filterByHairColor(hairColor, gnomes){
+    var gnomesFiltered = [];
+
+    gnomes.map((gnome =>{
+        if(gnome.hair_color === hairColor){
+            gnomesFiltered.push(gnome);
+        }
+    }));
+
+    return gnomesFiltered;
 }
 
 //SEARCH SORT==============================================================================
